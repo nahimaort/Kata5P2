00dcec0eec55784b5b6f490d6a1dc3032de4af3e
nahimaort <nahimaortega@gmail.com>
2021-11-05 10:41:01 +0000

    Kata4 versión 2

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -8,13 +8,29 @@ import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 
 public class Kata4 {
-    public static void main(String[] args) {
-        
+    public static List<Mail> input() {
         String nameFile = "email.txt";
         List<Mail> mailList = MailListReader.read(nameFile);
+        return mailList;
+    }
+    
+    public static Histogram<String> process(List<Mail> mailList) {
         Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-        
+        return histogram;
+    }
+    
+    public static void output(Histogram<String> histogram) {
         HistogramDisplay histogramDisplay = new HistogramDisplay("HistogramDisplay", histogram);
         histogramDisplay.execute();
+    }
+    
+    public static void execute() {
+        List<Mail> mailList = input();
+        Histogram<String> histogram = process(mailList);
+        output(histogram);
+    }
+    
+    public static void main(String[] args) {
+        Kata4.execute();
     }
 }
